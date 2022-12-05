ec230b2a28a1639b77bee6e272daa5ea2fe27b1f
Enzo-belardo <vincenzobelardo10@gmail.com>
2022-12-05 21:00:46 +0100

    terzo snack

--- snack-3.js
+++ snack-3.js
@@ -6,3 +6,87 @@
  * Calcola quanto pesano tutte le zucchine.
  *
  */
+
+const zucchine = [
+    {
+        varieta : 'romana',
+        peso : 20,
+        lunghezza: 15,
+
+    },
+        
+    
+    {
+        varieta : 'napoletana',
+        peso : 20,
+        lunghezza: 15,
+        
+    },
+
+    {
+        varieta : 'abruzzese',
+        peso : 17,
+        lunghezza: 15,
+         
+    },
+
+    {
+        varieta : 'pugliese',
+        peso : 26,
+        lunghezza: 15,
+        
+    },
+
+    {
+        varieta : 'palermitana',
+        peso : 23,
+        lunghezza: 15,
+        
+    },
+
+    {
+        varieta : 'veneta',
+        peso : 23,
+        lunghezza: 15,
+        
+    },
+
+    {
+        varieta : 'bolognese',
+        peso : 30,
+        lunghezza: 15,
+       
+    },
+
+    {
+        varieta : 'ligura',
+        peso : 25,
+        lunghezza: 15,
+        
+    },
+
+    {
+        varieta : 'milanese',
+        peso : 22,
+        lunghezza:  15,
+        
+    },
+
+    {
+        varieta : 'piemontese',
+        peso : 17,
+        lunghezza: 15,
+        
+    },
+
+
+]   
+
+let pesoTotale = 0
+
+for(let i = 0; i < zucchine.length; i++){
+    pesoTotale = pesoTotale + zucchine[i].peso;
+
+}
+console.log(pesoTotale)
+
